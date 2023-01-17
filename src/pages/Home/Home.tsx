425762869aa92c618a496dcd18567523633b8570
Роман Рымарчук <roman.rymarchuk01@gmail.com>
2023-01-17 22:18:13 +0200

    feature/add Home Page

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,10 @@
+import { Box } from "@mui/material";
+import { CardContainer, Header } from "../../components";
+
+const Home = () => (
+   <Box sx={{ m: "50px 75px", px: 0 }}>
+      <Header />
+      <CardContainer />
+   </Box>
+);
+export default Home;
