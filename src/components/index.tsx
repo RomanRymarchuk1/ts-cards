652c4a0005dd42182ce32d986638c489d08649bc
Роман Рымарчук <roman.rymarchuk01@gmail.com>
2023-01-17 22:21:49 +0200

    add for reexport

---
+++ src/components/index.tsx
@@ -0,0 +1,5 @@
+import Header from "./Header/Header";
+import CardContainer from "./CardContainer/CardContainer";
+import CustomButton from "./CustomButton/CustomButton";
+
+export { Header, CardContainer, CustomButton };
