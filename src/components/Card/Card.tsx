fe5a6cb32da0191e5e4c060a5fbc0162744147a2
Роман Рымарчук <roman.rymarchuk01@gmail.com>
2023-01-18 23:47:19 +0200

    fix/ refactor and use API data

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,9 +1,11 @@
 import { CardContent, CardMedia, Card as MuiCard, Typography, styled } from "@mui/material";
 import { ReactComponent as Arrow } from "./assets/ArrowRight.svg";
 import { ReactComponent as Date } from "./assets/Date.svg";
-
 import { CustomButton } from "..";
 import { Stack } from "@mui/system";
+import { textReduction } from "../../utils/textReduction";
+import { useNavigate } from "react-router-dom";
+import dateConvesion from "../../utils/dateСonversion";
 
 const CardWrapper = styled(MuiCard)(() => ({
    width: "400px",
@@ -13,46 +15,37 @@ const CardWrapper = styled(MuiCard)(() => ({
    boxSizing: "border-box",
 }));
 
-const Img = styled(CardMedia)(() => ({
-   width: "100%",
-   height: "217px",
-}));
+const Card = ({ cardData }: any) => {
+   const navigate = useNavigate();
+   const { title, id, imageUrl, summary, publishedAt }: any = cardData;
 
-const ContentContainer = styled(CardContent)(() => ({
-   padding: "25px",
-}));
+   const onClick = (): void => {
+      navigate(`/${id}`);
+   };
 
-const Title = styled(Typography)(() => ({
-   paddingTop: "25px",
-}));
-
-const Description = styled(Typography)(() => ({
-   paddingTop: "20px",
-}));
-
-const Card = (props: any) => {
-   const { title, imageUrl, summary, publishedAt } = props;
    return (
       <CardWrapper>
-         <Img image="https://4lapy.ru/resize/1664x1000/upload/medialibrary/270/2703fd71a17c0843c0b91bbe28c4fe0f.jpg" />
-         <ContentContainer>
+         <CardMedia sx={{ width: "100%", height: "217px" }} image={imageUrl} />
+         <CardContent sx={{ padding: "25px" }}>
             <Stack sx={{ flexDirection: "row", alignItems: "center", gap: "10px" }}>
                <Date />
                <Typography variant="subtitle1" component="p">
-                  2222 22 22
+                  {dateConvesion(publishedAt)}
                </Typography>
             </Stack>
 
-            <Title variant="h2">Lorem ipsum dolor sit amet.</Title>
+            <Typography sx={{ paddingTop: "25px" }} variant="h2">
+               {title}
+            </Typography>
 
-            <Description variant="body1">
-               Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, perspiciatis maiores rerum maxime
-               dolore minus soluta, voluptatibus, quam corrupti ab repellendus? Vero quod a, quaerat magnam quisquam
-               fuga qui sit.
-            </Description>
+            <Typography sx={{ paddingTop: "20px" }} variant="body1">
+               {textReduction(summary, 100)}
+            </Typography>
 
-            <CustomButton mt="20px">Read more {<Arrow />}</CustomButton>
-         </ContentContainer>
+            <CustomButton onClick={onClick} m="20px 0 0 0">
+               Read more {<Arrow />}
+            </CustomButton>
+         </CardContent>
       </CardWrapper>
    );
 };
