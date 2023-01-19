5cc3616e09565453998addab22092794b34746ef
Роман Рымарчук <roman.rymarchuk01@gmail.com>
2023-01-19 23:39:02 +0200

    refactor/ remove all wrappers to App.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,7 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import theme from "./theme";
-import store from "./store";
-import { ThemeProvider } from "@mui/material/styles";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
+import "./index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
-root.render(
-   <ThemeProvider theme={theme}>
-      <Provider store={store}>
-         <BrowserRouter>
-            <App />
-         </BrowserRouter>
-      </Provider>
-   </ThemeProvider>
-);
+root.render(<App />);
