38acb4f1537eaa426bbbff48cb357b0f49160b2b
Роман Рымарчук <roman.rymarchuk01@gmail.com>
2023-02-25 18:40:45 +0200

    fix/ move providers to index.ts file

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,5 @@
 import AppRoutes from "./AppRoutes";
-import theme from "./theme";
-import store from "./store";
-import { ThemeProvider } from "@mui/material/styles";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
 
-const App = () => (
-   <ThemeProvider theme={theme}>
-      <Provider store={store}>
-         <BrowserRouter>
-            <AppRoutes />
-         </BrowserRouter>
-      </Provider>
-   </ThemeProvider>
-);
+const App = () => <AppRoutes />;
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,20 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.scss";
+import theme from "./theme";
+import store from "./store";
+import { ThemeProvider } from "@mui/material/styles";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
-root.render(<App />);
+root.render(
+   <ThemeProvider theme={theme}>
+      <Provider store={store}>
+         <BrowserRouter>
+            <App />
+         </BrowserRouter>
+      </Provider>
+   </ThemeProvider>
+);
